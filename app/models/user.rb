9aea1aff82898351642cf023079cdc9f1e9b45f7
Rebecca Anne Sealfon <ras2198@columbia.edu>
2012-12-30 10:29:46 -0500

    Authenticates for OpenID

--- app/models/user.rb
+++ app/models/user.rb
@@ -127,6 +127,16 @@ class User < ActiveRecord::Base
     User.find_by_login(loggin)
   end
 
+  #Authenticates for OpenID
+  def self.find_for_open_id(access_token, signed_in_resource=nil)
+    data = access_token['user_info']
+    if user = User.find_by_email(data["email"])
+      user
+    else # Create a user with a stub password.
+      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+    end
+  end
+
   # Downcases email address
   def email=(value)
     write_attribute :email, (value && !value.empty? ? value.downcase : self.email)
