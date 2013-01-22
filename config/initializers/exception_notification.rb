5ed618539e743fc1c9772a230a3bc164b98241c4
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-01-22 16:06:20 -0500

    Commented out exception notification

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,12 +1,12 @@
 # Set environment variable NOTIFIER_EMAIL=whatever@whatever.com
 # Notifications will be sent from and to this email.
 
-if (email=ENV['NOTIFIER_EMAIL']).blank?
-  if Rails.env == 'production'
-    Rails.logger.fatal "No exception notification email set."
-    raise ArgumentError.new "No exception notification email set. See #{__FILE__}"
-  end
-else
+# if (email=ENV['NOTIFIER_EMAIL']).blank?
+#   if Rails.env == 'production'
+#     Rails.logger.fatal "No exception notification email set."
+#     raise ArgumentError.new "No exception notification email set. See #{__FILE__}"
+#   end
+# else
   ResearchMatch::Application.config.middleware.use(ExceptionNotifier, {
     :email_prefix         => '[RM Notifier] ',
     :sender_address       => "ResearchMatch Notifier <#{email}>",
@@ -14,4 +14,4 @@ else
     :sections             => %w( more ) + ExceptionNotifier::Notifier.default_sections
   })
   ExceptionNotifier::Notifier.prepend_view_path File.join(Rails.root, 'app', 'views')
-end
+# end
